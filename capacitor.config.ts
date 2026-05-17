d0632ed28605326d75c051a6d1892b51866023c4
cedar528 <chrisndungu2020@gmail.com>
2026-05-17 12:36:45 +0300

    feat: add Capacitor Android app shell

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.demicafrica.ride',
+  appName: 'Demic Ride',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
